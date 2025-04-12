8fe24c71afcd664c807ec55681bd43e7e975594b
luk3rr <luk3rr@gmail.com>
2025-04-12 08:58:58 -0300

    add basic project structure

---
+++ src/main/java/com/hotela/HotelaApplication.java
@@ -0,0 +1,13 @@
+package com.hotela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelaApplication.class, args);
+	}
+
+}
