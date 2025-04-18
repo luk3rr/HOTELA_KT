f56494bb8f448da09010c039cf50328a02bf01a0
luk3rr <luk3rr@gmail.com>
2025-04-18 18:35:32 -0300

    refactor: improve custom exceptions

--- src/main/java/com/hotela/error/RestExceptionHandler.java
+++ src/main/java/com/hotela/error/RestExceptionHandler.java
@@ -1,19 +1,21 @@
 package com.hotela.error;
 
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class RestExceptionHandler {
     @ExceptionHandler(HotelaException.class)
     public ResponseEntity<ErrorResponse> handleHotelaException(HotelaException e) {
+        ResponseStatus responseStatus =
+                AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class);
+
         HttpStatus status =
-                switch (e) {
-                    case HotelaException.ExampleNotFoundException __ -> HttpStatus.NOT_FOUND;
-                    default -> HttpStatus.INTERNAL_SERVER_ERROR;
-                };
+                responseStatus != null ? responseStatus.code() : HttpStatus.INTERNAL_SERVER_ERROR;
 
         return new ResponseEntity<>(new ErrorResponse(e.getCode(), e.getMessage()), status);
     }
