e95eaa0d65e860b65c2347ab65c2ecb6d93d3474
luk3rr <luk3rr@gmail.com>
2025-04-18 18:03:05 -0300

    fix: spotless formatting

--- src/main/java/com/hotela/error/RestExceptionHandler.java
+++ src/main/java/com/hotela/error/RestExceptionHandler.java
@@ -1,29 +1,37 @@
 package com.hotela.error;
 
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.Map;
-
 @ControllerAdvice
 public class RestExceptionHandler {
-    private static final Map<Class<? extends HotelaException>, HttpStatus> STATUS_MAP = Map.ofEntries(
-            Map.entry(HotelaException.CustomerNotFoundException.class, HttpStatus.NOT_FOUND),
-            Map.entry(HotelaException.PartnerNotFoundException.class, HttpStatus.NOT_FOUND),
-            Map.entry(HotelaException.HotelNotFoundException.class, HttpStatus.NOT_FOUND),
-            Map.entry(HotelaException.RoomNotFoundException.class, HttpStatus.NOT_FOUND),
-            Map.entry(HotelaException.BookingNotFoundException.class, HttpStatus.NOT_FOUND),
-            Map.entry(HotelaException.PaymentNotFoundException.class, HttpStatus.NOT_FOUND),
-            Map.entry(HotelaException.ReviewNotFoundException.class, HttpStatus.NOT_FOUND),
-            Map.entry(HotelaException.ExampleNotFoundException.class, HttpStatus.NOT_FOUND),
-            Map.entry(HotelaException.InvalidCredentialsException.class, HttpStatus.UNAUTHORIZED),
-            Map.entry(HotelaException.InvalidOrExpiredTokenException.class, HttpStatus.UNAUTHORIZED),
-            Map.entry(HotelaException.EmailAlreadyRegisteredException.class, HttpStatus.CONFLICT),
-            Map.entry(HotelaException.BookingAlreadyCancelledException.class, HttpStatus.CONFLICT),
-            Map.entry(HotelaException.InvalidDataException.class, HttpStatus.BAD_REQUEST)
-    );
+    private static final Map<Class<? extends HotelaException>, HttpStatus> STATUS_MAP =
+            Map.ofEntries(
+                    Map.entry(
+                            HotelaException.CustomerNotFoundException.class, HttpStatus.NOT_FOUND),
+                    Map.entry(HotelaException.PartnerNotFoundException.class, HttpStatus.NOT_FOUND),
+                    Map.entry(HotelaException.HotelNotFoundException.class, HttpStatus.NOT_FOUND),
+                    Map.entry(HotelaException.RoomNotFoundException.class, HttpStatus.NOT_FOUND),
+                    Map.entry(HotelaException.BookingNotFoundException.class, HttpStatus.NOT_FOUND),
+                    Map.entry(HotelaException.PaymentNotFoundException.class, HttpStatus.NOT_FOUND),
+                    Map.entry(HotelaException.ReviewNotFoundException.class, HttpStatus.NOT_FOUND),
+                    Map.entry(HotelaException.ExampleNotFoundException.class, HttpStatus.NOT_FOUND),
+                    Map.entry(
+                            HotelaException.InvalidCredentialsException.class,
+                            HttpStatus.UNAUTHORIZED),
+                    Map.entry(
+                            HotelaException.InvalidOrExpiredTokenException.class,
+                            HttpStatus.UNAUTHORIZED),
+                    Map.entry(
+                            HotelaException.EmailAlreadyRegisteredException.class,
+                            HttpStatus.CONFLICT),
+                    Map.entry(
+                            HotelaException.BookingAlreadyCancelledException.class,
+                            HttpStatus.CONFLICT),
+                    Map.entry(HotelaException.InvalidDataException.class, HttpStatus.BAD_REQUEST));
 
     @ExceptionHandler(HotelaException.class)
     public ResponseEntity<ErrorResponse> handleHotelaException(HotelaException e) {
